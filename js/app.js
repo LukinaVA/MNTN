73e932c1f50f73fbfd5dd27a9be76de4f1c5bfdd
LukinaVA <lukina.va@edu.spbstu.ru>
2021-02-02 22:52:57 +0300

    Adaptive laptop-xx | optimizing image loading

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
+(() => {
+    'use strict';
+    const objects = document.getElementsByClassName('async-image');
+    Array.from(objects).map((item) => {
+        const img = new Image();
+        img.src = item.dataset.src;
+        img.onload = () => {
+            item.classList.remove('async-image');
+            return item.nodeName === 'IMG' ?
+                item.src = item.dataset.src :
+                item.style.backgroundImage = `url(${item.dataset.src})`;
+        };
+    });
+})();
+
 let pageSlider = new Swiper('.page', {
     wrapperClass: 'page__wrapper',
     slideClass: 'page__screen',
